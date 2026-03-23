cc5e57708077601c3740fde8fada383fd25830cc
Saishooo <shohei01@icloud.com>
2026-03-23 23:27:01 +0900

    少々のui変更

--- src/header.tsx
+++ src/header.tsx
@@ -4,15 +4,13 @@
 export default async function Header() {
   return (
     <div className="w-full">
-      <div className="bg-red-200">
-        <div className="flex flex-col w-full ml-[10px]">
-          <div className="h-[30px] mt-[15px]">
-            <a className="font-bold text-2xl">MUSCLE MEMORY</a>
-          </div>
+      <div className="flex flex-col w-full ml-[10px]">
+        <div className="h-[30px] mt-[15px]">
+          <a className="font-bold text-2xl">MUSCLE MEMORY</a>
+        </div>
 
-          <div className="mt-[15px]">
-            <p className="font-bold text-base">ゲスト</p>
-          </div>
+        <div className="mt-[15px]">
+          <p className="font-bold text-base">ゲスト</p>
         </div>
       </div>
     </div>
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 // lib/prisma.ts
 //import { PrismaClient } from "@prisma/client/extension";    //非推奨らしい
-import { PrismaClient } from "@prisma/client";    //推奨らいし
+import { PrismaClient } from "@prisma/client"; //推奨らいし
 //import { PrismaClient } from "../generated/prisma/index.js";
 
 export const prisma = new PrismaClient();

c39105b71a3593e9bbd581ed38242a762846b4dd
Saishooo <shohei01@icloud.com>
2026-03-30 00:31:05 +0900

    プルダウンで種目の表示

--- src/app/record/input/page.tsx
+++ src/app/record/input/page.tsx
@@ -3,8 +3,10 @@
 
 import { getPrisma } from "@/lib/prisma";
 
-export default function recordTraining_page() {
+export default async function recordTraining_page() {
   const prisma = getPrisma();
+  const exerciseCategory = await prisma.exerciseCategory.findMany();
+  const exercises = await prisma.exercise.findMany();
 
   return (
     <div className="min-h-screen">
@@ -12,18 +14,38 @@ export default function recordTraining_page() {
         <a className="font-bold text-xl">記録</a>
 
         <form>
-          <div className="w-[300px] h-[350px] mt-[20px] rounded border border-gray-500">
+          <div className="w-[300px] h-[400px] mt-[20px] rounded border border-gray-500">
             <div className="flex items-center mt-[35px] ml-[10px]">
-              <a className="font-bold w-[px]">種目</a>
-              <input
-                name="event"
-                type="text"
-                placeholder=" 種目を入力"
+              <a className="font-bold w-[px]">部位</a>
+              <select
+                name="exerciseCategory"
                 className="w-[200px] mr-[10px] ml-auto border rounded"
-              />
+              >
+                <option value="">選択してください</option>
+                {exerciseCategory.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
             </div>
 
-            <div className="flex items-center mt-[60px] ml-[10px]">
+            <div className="flex items-center mt-[50px] ml-[10px]">
+              <a className="font-bold w-[px]">種目</a>
+              <select
+                name="exercises"
+                className="w-[200px] mr-[10px] ml-auto border rounded"
+              >
+                <option value="">選択してください</option>
+                {exercises.map((exercise) => (
+                  <option key={exercise.id} value={exercise.id}>
+                    {exercise.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex items-center mt-[50px] ml-[10px]">
               <a className="font-bold">重さ</a>
               <input
                 name="weight"
@@ -33,17 +55,20 @@ export default function recordTraining_page() {
               />
             </div>
 
-            <div className="flex items-center mt-[60px] ml-[10px]">
+            <div className="flex items-center mt-[50px] ml-[10px]">
               <a className="font-bold">回数</a>
-              <input
-                name="rep"
-                type="text"
-                placeholder=" 回数を入力"
+              <select
+                name="event"
                 className="w-[200px] mr-[10px] ml-auto border rounded"
-              />
+              >
+                <option value="">選択してください</option>
+                {Array.from({ length: 30 }, (_, i) => (
+                  <option key={i}>{i + 1}</option>
+                ))}
+              </select>
             </div>
 
-            <div className="flex justify-end mt-[60px] mr-[20px]">
+            <div className="flex justify-end mt-[50px] mr-[20px]">
               <button className="font-bold">記録</button>
             </div>
           </div>

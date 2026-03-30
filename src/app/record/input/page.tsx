5e8700d655b4b68047bb3cc8c4167e6d2a28bae0
Saishooo <shohei01@icloud.com>
2026-03-30 20:53:15 +0900

    カテゴリーを選んだらトレーニング種目を絞り込むようにしました

--- src/app/record/input/page.tsx
+++ src/app/record/input/page.tsx
@@ -1,7 +1,9 @@
 // src/app/record/input/page.tsx
-//トレーニング内容を記録するページ
+// Server Component Prismaからデータを受け取る
+//⚫︎Prismaはサーバー専用なので、ブラウザ側では使用することができない。なので、ファイルを分ける。
 
 import { getPrisma } from "@/lib/prisma";
+import RecordInputForm from "./recordInputForm";
 
 export default async function recordTraining_page() {
   const prisma = getPrisma();
@@ -9,71 +11,9 @@ export default async function recordTraining_page() {
   const exercises = await prisma.exercise.findMany();
 
   return (
-    <div className="min-h-screen">
-      <div className="flex flex-col items-center mt-[40px]">
-        <a className="font-bold text-xl">記録</a>
-
-        <form>
-          <div className="w-[300px] h-[400px] mt-[20px] rounded border border-gray-500">
-            <div className="flex items-center mt-[35px] ml-[10px]">
-              <a className="font-bold w-[px]">部位</a>
-              <select
-                name="exerciseCategory"
-                className="w-[200px] mr-[10px] ml-auto border rounded"
-              >
-                <option value="">選択してください</option>
-                {exerciseCategory.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="flex items-center mt-[50px] ml-[10px]">
-              <a className="font-bold w-[px]">種目</a>
-              <select
-                name="exercises"
-                className="w-[200px] mr-[10px] ml-auto border rounded"
-              >
-                <option value="">選択してください</option>
-                {exercises.map((exercise) => (
-                  <option key={exercise.id} value={exercise.id}>
-                    {exercise.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="flex items-center mt-[50px] ml-[10px]">
-              <a className="font-bold">重さ</a>
-              <input
-                name="weight"
-                type="text"
-                placeholder=" 重さを入力"
-                className="w-[200px] mr-[10px] ml-auto border rounded"
-              />
-            </div>
-
-            <div className="flex items-center mt-[50px] ml-[10px]">
-              <a className="font-bold">回数</a>
-              <select
-                name="event"
-                className="w-[200px] mr-[10px] ml-auto border rounded"
-              >
-                <option value="">選択してください</option>
-                {Array.from({ length: 30 }, (_, i) => (
-                  <option key={i}>{i + 1}</option>
-                ))}
-              </select>
-            </div>
-
-            <div className="flex justify-end mt-[50px] mr-[20px]">
-              <button className="font-bold">記録</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+    <RecordInputForm
+      exerciseCategory={exerciseCategory}
+      exercises={exercises}
+    />
   );
 }
---
+++ src/app/record/input/recordInputForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+// src/app/record/input/recordInputForm.tsx
+// Client Component ユーザー側の表示
+//⚫︎useStateはクライアント専用。これがファイルを分ける理由です。
+
+import { useState } from "react";
+
+//ExerciseCategoryテーブルの型定義
+type ExerciseCategory = {
+  id: number;
+  name: string;
+};
+
+//Exrcisesテーブルの型定義
+type Exercises = {
+  id: number;
+  name: string;
+  categoryId: number;
+};
+
+//RecordInputFormの引数の型定義
+type Props = {
+  exerciseCategory: ExerciseCategory[];
+  exercises: Exercises[];
+};
+
+export default function RecordInputForm({
+  exerciseCategory,
+  exercises,
+}: Props) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  return (
+    <div className="min-h-screen">
+      <div className="flex flex-col items-center mt-[40px]">
+        <a className="font-bold text-xl">記録</a>
+
+        <form>
+          <div className="w-[300px] h-[400px] mt-[20px] rounded border border-gray-500">
+            <div className="flex items-center mt-[35px] ml-[10px]">
+              <a className="font-bold w-[px]">部位</a>
+              <select
+                name="exerciseCategory"
+                className="w-[200px] mr-[10px] ml-auto border rounded"
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                <option value="">選択してください</option>
+                {exerciseCategory.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex items-center mt-[50px] ml-[10px]">
+              <a className="font-bold w-[px]">種目</a>
+              <select
+                name="exercises"
+                className="w-[200px] mr-[10px] ml-auto border rounded"
+              >
+                <option value="">選択してください</option>
+                {exercises
+                  .filter(
+                    (exercise) =>
+                      String(exercise.categoryId) === selectedCategory
+                  )
+                  .map((exercise) => (
+                    <option key={exercise.id} value={exercise.id}>
+                      {exercise.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            <div className="flex items-center mt-[50px] ml-[10px]">
+              <a className="font-bold">重さ</a>
+              <input
+                name="weight"
+                type="text"
+                placeholder=" 重さを入力"
+                className="w-[200px] mr-[10px] ml-auto border rounded"
+              />
+            </div>
+
+            <div className="flex items-center mt-[50px] ml-[10px]">
+              <a className="font-bold">回数</a>
+              <select
+                name="event"
+                className="w-[200px] mr-[10px] ml-auto border rounded"
+              >
+                <option value="">選択してください</option>
+                {Array.from({ length: 30 }, (_, i) => (
+                  <option key={i}>{i + 1}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex justify-end mt-[50px] mr-[20px]">
+              <button className="font-bold">記録</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}

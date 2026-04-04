c047dad2968af96f50ff8d3959578e79ee89e424
Saishooo <shohei01@icloud.com>
2026-04-04 19:37:49 +0900

    今日の筋トレ記録のみを表示できる様にしました

--- src/lib/record.ts
+++ src/lib/record.ts
@@ -1,12 +1,21 @@
 // src/lib/record.ts
 import { getPrisma } from "./prisma";
 
+//今日の記録を吸い上げる関数
 export async function getTodayRecordsByUserId(userId: number | null) {
   const prisma = getPrisma();
 
   if (!userId) {
     return [];
   }
+
+  //今日の開始(00:00:00)
+  const startOfToday = new Date();
+  startOfToday.setHours( 0, 0, 0, 0 );
+
+  //今日の終了(23:59:59)
+  const endOfToday = new Date();
+  endOfToday.setHours( 23, 59, 59, 9999 );
 
   //trainingからsessionに行き、userIdが一致するものを取得する
   return await prisma.training.findMany({
@@ -16,6 +25,10 @@ export async function getTodayRecordsByUserId(userId: number | null) {
           userId: userId,
         },
       },
+      createdAt: {
+        gte: startOfToday,
+        lte: endOfToday,
+      },
     },
     include: {
       exercise: true,
@@ -23,6 +36,7 @@ export async function getTodayRecordsByUserId(userId: number | null) {
   });
 }
 
+//全ての記録を吸い上げる関数
 export async function getAllRecordsByUserId(userId: number | null) {
   const prisma = getPrisma();
 

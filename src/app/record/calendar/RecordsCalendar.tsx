f9db726334da25c00aa1e427b2037e41e1c49edc
Saishooo <shohei01@icloud.com>
2026-06-09 22:18:40 +0900

    時間が新しいものから表示する

--- src/app/record/calendar/RecordsCalendar.tsx
+++ src/app/record/calendar/RecordsCalendar.tsx
@@ -38,6 +38,10 @@ export default function RecordsCalendar({ trainings, userId }: Props) {
     );
   });
 
+  const sortedTrainings = [...selectedTrainings].sort(
+    (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+  );
+
   return (
     <div className="flex justify-center">
       <div className="bg-white p-5 rounded-xl shadow-lg">
@@ -58,7 +62,7 @@ export default function RecordsCalendar({ trainings, userId }: Props) {
         </p>
 
         <div className="pt-2">
-          {selectedTrainings.length === 0 ? (
+          {sortedTrainings.length === 0 ? (
             <p className="flex justify-center items-center h-30 text-center text-gray-500">
               この日の記録はありません
             </p>
@@ -69,7 +73,7 @@ export default function RecordsCalendar({ trainings, userId }: Props) {
                 <p className="w-16 text-center font-bold">重量</p>
                 <p className="w-16 text-center font-bold">回数</p>
               </div>
-              {selectedTrainings.map((training) => (
+              {sortedTrainings.map((training) => (
                 <div key={training.id} className="flex w-full">
                   <p className="w-62 pl-2">{training.exercise.name}</p>
                   <p className="w-14 pl-2 text-center">{training.weight}</p>

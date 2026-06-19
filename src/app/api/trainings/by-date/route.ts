77c45e36072310c7db5d03d0b6d39fd0ca5918be
Saishooo <shohei01@icloud.com>
2026-06-19 23:15:49 +0900

    DB検索の際の実行環境依存解消

--- src/app/api/trainings/by-date/route.ts
+++ src/app/api/trainings/by-date/route.ts
@@ -3,25 +3,33 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import { getPrisma } from "@/lib/prisma";
+import { error } from "console";
+import { fromZonedTime } from "date-fns-tz";
+import { format, addDays, parseISO } from "date-fns";
 
 export async function GET(req: NextRequest) {
   const prisma = getPrisma();
 
   const userId = req.nextUrl.searchParams.get("userId");
   const date = req.nextUrl.searchParams.get("date");
+  const timeZone = req.nextUrl.searchParams.get("timeZone");
 
-  const selectedDate = new Date(date!);
+  if (!userId || !date || !timeZone) {
+    return NextResponse.json(
+      { error: "userId, date, timeZone are required" },
+      { status: 400 }
+    );
+  }
 
-  const start = new Date(selectedDate);
-  start.setHours(0, 0, 0, 0);
+  const start = fromZonedTime(`${date} 00:00:00`, timeZone);
 
-  const end = new Date(start);
-  end.setDate(end.getDate() + 1);
+  const nextDateText = format(addDays(parseISO(date), 1), "yyyy-MM-dd");
+  const end = fromZonedTime(`${nextDateText} 00:00:00`, timeZone);
 
   const trainings = await prisma.training.findMany({
     where: {
       session: {
-        userId: userId!,
+        userId: userId,
       },
       createdAt: {
         gte: start,
